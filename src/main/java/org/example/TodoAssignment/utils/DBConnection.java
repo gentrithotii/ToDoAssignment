2d8fcb998d69e3d4dcca0cc2c6e2b62203d14371
Gentrithotii <Gentrithoti@hotmail.com>
2025-05-20 14:45:10 +0200

    Added .env file for the url and pass and username for DB imported dependencies for the .env for java

--- src/main/java/org/example/TodoAssignment/utils/DBConnection.java
+++ src/main/java/org/example/TodoAssignment/utils/DBConnection.java
@@ -1,16 +1,29 @@
 package org.example.TodoAssignment.utils;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
     private static DBConnection instance;
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/todoit";
-    private static final String USER = "root";
-    private static final String PASSWORD = "1234";
+    private String url;
+    private String user;
+    private String password;
 
     private DBConnection() {
+        Dotenv dotenv = Dotenv.load();
+        this.url = dotenv.get("DB_URL");
+        this.user = dotenv.get("DB_USER");
+        this.password = dotenv.get("DB_PASSWORD");
+
+        if (url == null || user == null || password == null) {
+            throw new RuntimeException("No db environment variables in .env file ");
+        }
     }
 
     public static synchronized DBConnection getInstance() {
@@ -24,7 +37,18 @@ public class DBConnection {
         return instance;
     }
 
+//    private void loadDBConfigFromFile() {
+//        String filePath = "src/main/resources/file";
+//        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+//            this.url = reader.readLine();
+//            this.user = reader.readLine();
+//            this.password = reader.readLine();
+//        } catch (IOException e) {
+//            throw new RuntimeException("Failed to load file", e);
+//        }
+//    }
+
     public Connection getConnection() throws SQLException {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(url, user, password);
     }
 }
